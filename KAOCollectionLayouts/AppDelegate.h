30ee640986c5bee88a93c510cec3ade16ecc38e4
Andrey Kravchenko <kievkao@gmail.com>
2015-08-17 12:57:33 +0300

    Initial Commit

---
+++ KAOCollectionLayouts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KAOCollectionLayouts
+//
+//  Created by Andrey Kravchenko on 8/17/15.
+//  Copyright (c) 2015 Kievkao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KAOCollectionLayouts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KAOCollectionLayouts
+//
+//  Created by Andrey Kravchenko on 8/17/15.
+//  Copyright (c) 2015 Kievkao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
