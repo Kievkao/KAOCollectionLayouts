24bc67e89afb08405afac989ec33f87fcae4f784
Andrey Kravchenko <kievkao@gmail.com>
2015-08-17 13:37:07 +0300

    Create layout class

---
+++ KAOCollectionLayouts/KAOPickerLayout.h
@@ -0,0 +1,13 @@
+//
+//  KAOPickerLayout.h
+//  KAOCollectionLayouts
+//
+//  Created by Andrey Kravchenko on 8/17/15.
+//  Copyright (c) 2015 Kievkao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAOPickerLayout : UICollectionViewFlowLayout
+
+@end
---
+++ KAOCollectionLayouts/KAOPickerLayout.m
@@ -0,0 +1,13 @@
+//
+//  KAOPickerLayout.m
+//  KAOCollectionLayouts
+//
+//  Created by Andrey Kravchenko on 8/17/15.
+//  Copyright (c) 2015 Kievkao. All rights reserved.
+//
+
+#import "KAOPickerLayout.h"
+
+@implementation KAOPickerLayout
+
+@end
